165a352426fd545b68de2facdab005ac95485339
mrcommand3xp <136920696+mrcommand3xp@users.noreply.github.com>
2023-06-18 07:45:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #tidak saya copy ke bin agar berguna di semua terminal kayak ny :v
-go install -v go install github.com/hahwul/dalfox/v2@latest
+go install -v github.com/hahwul/dalfox/v2@latest
 sudo cp ~/go/bin/* /usr/bin/
 sudo cp ~/go/bin/* /usr/local/bin/
 figlet Done
